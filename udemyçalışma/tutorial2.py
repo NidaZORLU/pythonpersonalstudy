942c6d808bfa2c527e7c6bdd37572ed3a9bc99b4
NidaZORLU <nidazorlunz@hotmail.com>
2022-11-17 11:20:48 +0300

    yorum satırı ekledim konu anlatımı kısmı

---
+++ udemyçalışma/tutorial2.py
@@ -0,0 +1,17 @@
+"""
+class Person:#class nesne üretir,nesnenin her biri de örnek oldupundan örnek üretir
+    pass
+
+class Person():#yukardaki ile aynı şeydir
+    pass
+
+class Person(object):#yukaridakiler ile aynı şeydir
+    pass
+"""
+"""
+class nameOfClass(SuperClass):
+    __init__
+    __str__
+    attributes(age,address,name...) #variable yerine kullanılır
+    methods #function yerine kullanılır
+"""
