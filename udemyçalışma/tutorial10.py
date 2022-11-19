a077ea4eb28dc95021092a7b2e896d457ade9b18
NidaZORLU <nidazorlunz@hotmail.com>
2022-11-19 16:05:39 +0300

    yorumsatırı eklendi ve tutorial10 tamamlandı

---
+++ udemyçalışma/tutorial10.py
@@ -0,0 +1,10 @@
+class Person:
+    @staticmethod
+    def static_function(): #sınıfa aitken cls yazılıs örneğe aitken self yazılır,boşken ikisinede ait değildir
+        print('static method')
+
+#Person.static_function()
+"""python da bir dizi özel yöntemvardır
+tüm bu özel yöntemler çift alt çubukla başlar ve biter
+çok gerekmedikçe kendimiz tanımlamamalıyız başka bir kod ile çakışabilir
+"""
